f6597719ee95a1880876a2edb1ac98686e877d30
Soroush <soroush.raw@gmail.com>
2022-12-07 11:27:18 +0100

    validation form with customHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-import Input from "./components/Input";
+import Form from "./components/Form";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="app">
-      <Input />
+      <Form/>
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,92 @@
+import useInput from "../Hooks/use-input";
+
 const Form = (props) => {
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameHandeler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: firstNameReset,
+  } = useInput((value) => value.trim() !== "");
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameHandeler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: lastNameReset,
+  } = useInput((value) => value.trim() !== "");
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailHandeler,
+    inputBlurHandler: emailBlurHandler,
+    reset: emailReset,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    if (!formIsValid) {
+      return;
+    }
+    console.log("submited!");
+    console.log(firstNameValue, lastNameValue, emailValue);
+
+    firstNameReset(), lastNameReset(), emailReset();
+  };
+  const firstNameClasses = firstNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const lastNameClasses = lastNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="control-group">
+        <div className={firstNameClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={firstNameValue}
+            onChange={firstNameHandeler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameHasError && <p>please enter your first name</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastNameClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            value={lastNameValue}
+            onChange={lastNameHandeler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameHasError && <p>please enter your first name</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          value={emailValue}
+          onChange={emailHandeler}
+          onBlur={emailBlurHandler}
+        />
+        {emailHasError && <p>please enter valid email adresse</p>}
       </div>
-      <div className='form-actions'>
-        <button>Sublmit</button>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Sublmit</button>
       </div>
     </form>
   );
