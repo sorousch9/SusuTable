c75446090dde325939c5dbe01a494ce0ee0dcffe
Sorousch <soroush.raw@gmail.com>
2023-04-25 11:29:22 +0200

    Add navigation and chart options
    Refactor components to enable multiple
    charts to be displayed and included
    within routes
    
    * Add PieChart, LineChart, AreaChart,
    BarChart, ComboChart, FlowChart and
    DoughnutChart components
    * Add React Router paths for each chart
    * Add NavChart for navigation between
    charts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import TableFC from "./components/Table";
 import DataSelection from "./components/DataSelection";
 import NavChart from "./components/NavChart";
 import { Container, Col, Row } from "react-bootstrap";
-import Charts from "./components/Charts";
+import PieChart from "./components/Charts/PieChart";
 import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import LineChart from "./components/Charts/LineChart";
+import AreaChart from "./components/Charts/AreaChart";
+import BarChart from "./components/Charts/BarChart";
+import ComboChart from "./components/Charts/ComboChart";
+import FlowChart from "./components/Charts/FlowChart";
+import DoughnutChart from "./components/Charts/DoughnutChart";
 
 export interface Column {
   name: string;
@@ -24,6 +31,7 @@ const API_ROUTES = {
 };
 
 function App() {
+  //name:Header and fieldName:accessorKey
   const [columns, setColumns] = useState<Column[]>([]);
   const [data, setData] = useState<DataRow[]>([]);
 
@@ -49,7 +57,15 @@ function App() {
       <Row>
         <Col xs={9}>
           <NavChart />
-          <Charts />
+          <Routes>
+            <Route path="/" element={<PieChart />} />
+            <Route path="/lineChart" element={<LineChart />} />
+            <Route path="/areaChart" element={<AreaChart />} />
+            <Route path="/barChart" element={<BarChart />} />
+            <Route path="/comboChart" element={<ComboChart />} />
+            <Route path="/flowChart" element={<FlowChart />} />
+            <Route path="/doughnutChart" element={<DoughnutChart />} />
+          </Routes>
           <TableFC columns={columns} data={data} />
         </Col>
         <Col xs={3}>
