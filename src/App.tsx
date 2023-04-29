026a126303866a8244de220a42601f1fa36d820a
Sorousch <soroush.raw@gmail.com>
2023-04-29 22:17:15 +0200

    refactor: consolidate chart components
    Provide a single charting component by removing
    DataSelection, NavChart, and individual chart
    components from src/App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
 import "./App.css";
 import TableAPI from "./components/Table";
-import DataSelection from "./components/DataSelection";
-import NavChart from "./components/NavChart";
-import { Container, Col, Row } from "react-bootstrap";
-import PieChart from "./components/Charts/PieChart";
-import { Routes, Route } from "react-router-dom";
-import LineChart from "./components/Charts/LineChart";
-import AreaChart from "./components/Charts/AreaChart";
-import BarChart from "./components/Charts/BarChart";
-import Histogram from "./components/Charts/Histogram";
+import Charts from "./components/Charts";
+import { Container, Row } from "react-bootstrap";
 
 const App = () => {
   return (
     <Container fluid>
       <Row>
-        <Col xs={9}>
-          <NavChart />
-          <Routes>
-            <Route path="/" element={<PieChart />} />
-            <Route path="/lineChart" element={<LineChart />} />
-            <Route path="/areaChart" element={<AreaChart />} />
-            <Route path="/barChart" element={<BarChart />} />
-            <Route path="/histogram" element={<Histogram />} />
-          </Routes>
-        </Col>
-        <Col xs={3}>
-          <DataSelection />
-        </Col>
+        <Charts />
       </Row>
       <Row>
         <TableAPI />
