ec0eb50c9221e362fa80bd8c0b230f2d20a079a0
Sorousch <soroush.raw@gmail.com>
2023-04-30 12:24:40 +0200

    refactor(Charts): reorganize components for better structure and readability

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,32 +1,18 @@
-import React, { useState } from "react";
-import { Col, Container, Form, InputGroup, Row } from "react-bootstrap";
-import Tooltip from "react-bootstrap/Tooltip";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-
-import { TfiHelpAlt } from "react-icons/tfi";
+import { Col, Container, Row } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import PieChart from "./Charts/PieChart";
 import LineChart from "./Charts/LineChart";
 import AreaChart from "./Charts/AreaChart";
 import BarChart from "./Charts/BarChart";
 import Histogram from "./Charts/Histogram";
-import NavChart from "./NavChart";
+import NavChart from "./Charts/NavChart";
+import Menu from "./Charts/Menu";
 export default function Charts() {
-  const [dimension, setDimension] = useState("");
-  const [measure, setMeasure] = useState("");
-
-  const handleChangeDimension = (event: any) => {
-    setDimension(event.target.value);
-  };
-  const handleChangeMeasure = (event: any) => {
-    setMeasure(event.target.value);
-  };
-
   return (
     <Container>
       <Row>
         <Col xs={9}>
-        <NavChart/>
+          <NavChart />
           <Routes>
             <Route path="/" element={<PieChart />} />
             <Route path="/lineChart" element={<LineChart />} />
@@ -36,45 +22,7 @@ export default function Charts() {
           </Routes>
         </Col>
         <Col xs={3}>
-          <OverlayTrigger
-            overlay={
-              <Tooltip>
-                A dimension is a field that orders, groups, or categorizes your
-                data, such as dates and categories. The dimension is often shown
-                on the x-axis or as points on a map.
-              </Tooltip>
-            }
-          >
-            <h4>
-              Dimension <TfiHelpAlt size={"1rem"} />
-            </h4>
-          </OverlayTrigger>
-          <Form>
-            <InputGroup>
-              <Form.Select value={dimension} onChange={handleChangeDimension}>
-                <option value={1}>Date</option>
-              </Form.Select>
-            </InputGroup>
-          </Form>
-          <OverlayTrigger
-            overlay={
-              <Tooltip>
-                A measure is a numeric field or the count of rows associated
-                with the selected dimension.
-              </Tooltip>
-            }
-          >
-            <h4>
-              Measure <TfiHelpAlt size={"1rem"} />
-            </h4>
-          </OverlayTrigger>
-          <Form>
-            <InputGroup>
-              <Form.Select value={measure} onChange={handleChangeMeasure}>
-                <option value={1}>Community Board</option>
-              </Form.Select>
-            </InputGroup>
-          </Form>
+          <Menu />
         </Col>
       </Row>
     </Container>
