2a202cab89a3f8d526b4838a65a6248a33525ba8
Sorousch <soroush.raw@gmail.com>
2023-05-01 16:56:45 +0200

    refactor(charts): restructure Menu component to use Accordion component.

--- src/components/Charts/Menu.tsx
+++ src/components/Charts/Menu.tsx
@@ -1,11 +1,10 @@
-import "./menu.css";
 import { useState } from "react";
-import Tooltip from "react-bootstrap/Tooltip";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import { TfiHelpAlt } from "react-icons/tfi";
-import { Form, InputGroup } from "react-bootstrap";
+import "./menu.css";
+import { Form, InputGroup, OverlayTrigger, Tooltip } from "react-bootstrap";
 
-const Menu = () => {
+const Accordion = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
   const [dimension, setDimension] = useState("");
   const [measure, setMeasure] = useState("");
 
@@ -16,49 +15,92 @@ const Menu = () => {
     setMeasure(event.target.value);
   };
 
+  const handleClick = (index: number) => {
+    if (activeIndex === index) {
+      setActiveIndex(0);
+    } else {
+      setActiveIndex(index);
+    }
+  };
+
+  const items = [
+    {
+      title: "Axles",
+      content: (
+        <>
+          <OverlayTrigger
+            overlay={
+              <Tooltip>
+                A dimension is a field that orders, groups, or categorizes your
+                data, such as dates and categories. The dimension is often shown
+                on the x-axis or as points on a map.
+              </Tooltip>
+            }
+          >
+            <h4>
+              Dimension <TfiHelpAlt size={"1rem"} />
+            </h4>
+          </OverlayTrigger>
+          <Form>
+            <InputGroup>
+              <Form.Select value={dimension} onChange={handleChangeDimension}>
+                <option value={1}>Date</option>
+              </Form.Select>
+            </InputGroup>
+          </Form>
+        </>
+      ),
+    },
+    {
+      title: "Filters",
+      content: (
+        <>
+          <OverlayTrigger
+            overlay={
+              <Tooltip>
+                A measure is a numeric field or the count of rows associated
+                with the selected dimension.
+              </Tooltip>
+            }
+          >
+            <h4>
+              Measure <TfiHelpAlt size={"1rem"} />
+            </h4>
+          </OverlayTrigger>
+          <Form>
+            <InputGroup>
+              <Form.Select value={measure} onChange={handleChangeMeasure}>
+                <option value={1}>Community Board</option>
+              </Form.Select>
+            </InputGroup>
+          </Form>
+        </>
+      ),
+    },
+  ];
   return (
-    <div>
-      <OverlayTrigger
-        overlay={
-          <Tooltip>
-            A dimension is a field that orders, groups, or categorizes your
-            data, such as dates and categories. The dimension is often shown on
-            the x-axis or as points on a map.
-          </Tooltip>
-        }
-      >
-        <h4>
-          Dimension <TfiHelpAlt size={"1rem"} />
-        </h4>
-      </OverlayTrigger>
-      <Form>
-        <InputGroup>
-          <Form.Select value={dimension} onChange={handleChangeDimension}>
-            <option value={1}>Date</option>
-          </Form.Select>
-        </InputGroup>
-      </Form>
-      <OverlayTrigger
-        overlay={
-          <Tooltip>
-            A measure is a numeric field or the count of rows associated with
-            the selected dimension.
-          </Tooltip>
-        }
-      >
-        <h4>
-          Measure <TfiHelpAlt size={"1rem"} />
-        </h4>
-      </OverlayTrigger>
-      <Form>
-        <InputGroup>
-          <Form.Select value={measure} onChange={handleChangeMeasure}>
-            <option value={1}>Community Board</option>
-          </Form.Select>
-        </InputGroup>
-      </Form>
+    <div className="accordion">
+      {items.map((item, index) => (
+        <div key={index} className="accordion-item">
+          <div
+            className={`accordion-header ${
+              activeIndex === index ? "active" : ""
+            }`}
+            onClick={() => handleClick(index)}
+          >
+            {item.title}
+          </div>
+          <div
+            className={`accordion-content ${
+              activeIndex === index ? "active" : ""
+            }`}
+          >
+            {item.content}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Menu;
+export default Accordion;
