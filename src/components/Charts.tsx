1eb2b0793c25f76228164e8db6937ebfb662af29
Sorousch <soroush.raw@gmail.com>
2023-05-03 09:41:34 +0200

    charts updated

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import PieChart from "./Charts/PieChart";
@@ -7,22 +8,26 @@ import BarChart from "./Charts/BarChart";
 import Histogram from "./Charts/Histogram";
 import NavChart from "./Charts/NavChart";
 import Menu from "./Charts/Menu";
+import { DataItem } from "../../types/charts";
+
 export default function Charts() {
+  const [axlesData, setAxlesData] = useState<DataItem[]>([]);
+
   return (
     <Container>
       <Row>
         <Col xs={9}>
           <NavChart />
           <Routes>
-            <Route path="/" element={<PieChart />} />
-            <Route path="/lineChart" element={<LineChart />} />
+            <Route path="/pieChart" element={<PieChart />} />
+            <Route path="/" element={<LineChart axlesData={axlesData}/>} />
             <Route path="/areaChart" element={<AreaChart />} />
             <Route path="/barChart" element={<BarChart />} />
             <Route path="/histogram" element={<Histogram />} />
           </Routes>
         </Col>
         <Col xs={3}>
-          <Menu />
+          <Menu setAxlesData={setAxlesData} />
         </Col>
       </Row>
     </Container>
