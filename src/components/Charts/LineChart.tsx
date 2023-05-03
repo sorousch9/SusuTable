3fbe8a834f40b348054e297184f597aa0c0df127
Sorousch <soroush.raw@gmail.com>
2023-05-03 12:48:53 +0200

    LineChart updated

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,25 +1,48 @@
 import { FC } from "react";
-import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryVoronoiContainer,
+} from "victory";
 import { DataProps } from "../../../types/charts";
 
 interface ChartDataPoint {
   x: string | number;
+  label: string | number;
   y: number;
 }
 
 const LineChart: FC<DataProps> = ({ axlesData }) => {
-
-  const dataPoints: ChartDataPoint[] = axlesData.map(
-    ({ __dimension_alias__, __measure_alias__ }) => ({
-      x: __dimension_alias__,
+  const dataPoints: ChartDataPoint[] = axlesData
+    .filter(
+      ({ __dimension_alias__, __measure_alias__ }) =>
+        __dimension_alias__ && __measure_alias__
+    )
+    .map(({ __dimension_alias__, __measure_alias__ }) => ({
+      x: String(__dimension_alias__),
       y: Number(__measure_alias__),
-    })
-  );
+      label: `${__measure_alias__}, ${__dimension_alias__}`,
+    }));
 
   return (
     <div className="charts">
-      <VictoryChart theme={VictoryTheme.material}>
-        <VictoryLine data={dataPoints} />
+      <VictoryChart
+        theme={VictoryTheme.material}
+        animate={{
+          duration: 2000,
+          onLoad: { duration: 1000 },
+        }}
+        width={800}
+        containerComponent={<VictoryVoronoiContainer />}
+      >
+        <VictoryLine
+          labelComponent={<VictoryTooltip />}
+          data={dataPoints}
+          interpolation="step"
+          x={""}
+        />
       </VictoryChart>
     </div>
   );
