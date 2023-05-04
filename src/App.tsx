81428bae135a3ba34b09bfff2bd1f45031f29e59
Sorousch <soroush.raw@gmail.com>
2023-05-04 18:19:41 +0200

    using columns state in parent camponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import TableAPI from "./components/Table";
 import Charts from "./components/Charts";
 import { Container, Row } from "react-bootstrap";
+import axios from "axios";
+import { Column } from "../types/tableTypes";
 
 const App = () => {
+  const [columns, setColumns] = useState<Column[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://data.cityofnewyork.us/api/views/xnfm-u3k5.json")
+      .then((response) => {
+        setColumns(response.data.columns);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <Container fluid>
       <Row>
-        <Charts />
+        <Charts columns={columns} />
       </Row>
       <Row>
-        <TableAPI />
+        <TableAPI columns={columns} setColumns={setColumns} />
       </Row>
     </Container>
   );
