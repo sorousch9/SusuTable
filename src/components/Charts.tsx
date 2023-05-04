af3aebbcd39dd1437f377fd4eabc7e3213e2f7c4
Sorousch <soroush.raw@gmail.com>
2023-05-04 18:20:46 +0200

    rewrite charts x and y

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,35 +1,62 @@
-import { useState } from "react";
+import { useState, FC } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
-import PieChart from "./Charts/PieChart";
-import LineChart from "./Charts/LineChart";
-import AreaChart from "./Charts/AreaChart";
-import BarChart from "./Charts/BarChart";
+import PeiChartE from "./Charts/PieChart";
+import LineChartE from "./Charts/LineChart";
+import AreaChartE from "./Charts/AreaChart";
+import BarChartE from "./Charts/BarChart";
 import Histogram from "./Charts/Histogram";
 import NavChart from "./Charts/NavChart";
 import Menu from "./Charts/Menu";
-import { DataItem } from "../../types/charts";
+import { ChartDataPoint, DataItem } from "../../types/chartsTypes";
+import { PropsColumns } from "../../types/tableTypes";
+import ScatterChartE from "./Charts/ScatterChart";
 
-export default function Charts() {
+const Charts: FC<PropsColumns> = ({ columns }) => {
   const [axlesData, setAxlesData] = useState<DataItem[]>([]);
-
+  const dataPoints: ChartDataPoint[] = axlesData
+    .filter(
+      ({ __dimension_alias__, __measure_alias__ }) =>
+        __dimension_alias__ && __measure_alias__
+    )
+    .map(({ __dimension_alias__, __measure_alias__ }) => ({
+      x: String(__dimension_alias__),
+      y: Number(__measure_alias__),
+    }));
   return (
     <Container>
       <Row>
         <Col xs={9}>
           <NavChart />
           <Routes>
-            <Route path="/pieChart" element={<PieChart />} />
-            <Route path="/" element={<LineChart axlesData={axlesData}/>} />
-            <Route path="/areaChart" element={<AreaChart />} />
-            <Route path="/barChart" element={<BarChart />} />
-            <Route path="/histogram" element={<Histogram />} />
+            <Route
+              path="/pieChart"
+              element={<PeiChartE dataPoints={dataPoints} />}
+            />
+             <Route
+              path="/scatterChart"
+              element={<ScatterChartE dataPoints={dataPoints} />}
+            />
+            <Route path="/" element={<LineChartE dataPoints={dataPoints} />} />
+            <Route
+              path="/areaChart"
+              element={<AreaChartE dataPoints={dataPoints} />}
+            />
+            <Route
+              path="/barChart"
+              element={<BarChartE dataPoints={dataPoints} />}
+            />
+            <Route
+              path="/histogram"
+              element={<Histogram dataPoints={dataPoints} />}
+            />
           </Routes>
         </Col>
         <Col xs={3}>
-          <Menu setAxlesData={setAxlesData} />
+          <Menu setAxlesData={setAxlesData} columns={columns} />
         </Col>
       </Row>
     </Container>
   );
-}
+};
+export default Charts;
