f9e2d822f741dd415306d95756c03b735beba9f9
Sorousch <soroush.raw@gmail.com>
2023-05-10 12:54:14 +0200

    Add React.StrictMode to src/index.tsx
    Rendering React application inside React.StrictMode
    to get additional checks and warnings in development.

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
