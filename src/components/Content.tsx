5bff1bde004c7825edc857e6e88151688c6c165e
Sorousch <soroush.raw@gmail.com>
2023-05-11 12:45:13 +0200

    Remove navigation images from Content component
    Remove navigation images from Content component
    and adjust css forContentWrapper for screens
    smaller than 768px

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,22 +4,11 @@ import "./content.css";
 import Charts from "./Chart/Charts";
 import { PropsColumns } from "../../types/tableTypes";
 import Map from "./Map/Map";
-import { Route, Routes, Link } from "react-router-dom";
-import map from "../assets/map.webp";
-import chart from "../assets/chart.webp";
+import { Route, Routes } from "react-router-dom";
 
 const Content: FC<PropsColumns> = ({ columns }) => {
   return (
     <Row>
-      <div className="navigationContainer">
-        <Link to="/chart">
-          <img src={chart} alt="Chart" className="navigationLinksIcon" />
-        </Link>
-        <Link to="/">
-          <img src={map} alt="Map" className="navigationLinksIcon" />
-        </Link>
-      </div>
-
       <div className="contentWrapper">
         <Routes>
           <Route path="/chart/*" element={<Charts columns={columns} />} />
