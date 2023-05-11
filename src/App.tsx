482713537153740da6b70a5ff2c46ceeb3d4b6f1
Sorousch <soroush.raw@gmail.com>
2023-05-11 11:57:21 +0200

    Add Header component and related state
    Add Header component with associated state to improve
    the App's front-end look and feel.
    Update API endpoint call with more specific parameters.

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,20 @@ import { Container, Row } from "react-bootstrap";
 import axios from "axios";
 import { Column } from "../types/tableTypes";
 import Content from "./components/Content";
+import Header from "./components/Header";
+import { HeaderType } from "../types/headerType";
 
 const App = () => {
   const [columns, setColumns] = useState<Column[]>([]);
+  const [appDescription, setAppDescription] = useState<HeaderType>();
   useEffect(() => {
     axios
-      .get("https://data.cityofnewyork.us/api/views/if26-z6xq.json?read_from_nbe=true&version=2.1")
+      .get(
+        "https://data.cityofnewyork.us/api/views/if26-z6xq.json?read_from_nbe=true&version=2.1"
+      )
       .then((response) => {
         setColumns(response.data.columns);
+        setAppDescription(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -20,6 +26,7 @@ const App = () => {
   }, []);
   return (
     <Container fluid>
+      <Header appDescription={appDescription} />
       <Row>
         <Content columns={columns} />
       </Row>
