a7a42ec3b8eb265dc85af44edd06761977be24c3
Sorousch <soroush.raw@gmail.com>
2023-05-11 12:45:23 +0200

    docs: update Header component
    Updated Header component based on the new design.
    Updated the styles for the component in header.css.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
 import "./header.css";
 import logo from "../assets/logo.png";
 import { PropsHeader } from "../../types/headerType";
+import { NavLink } from "react-router-dom";
+import map from "../assets/map.webp";
+import chart from "../assets/chart.webp";
 
 const Header = ({ appDescription }: PropsHeader) => {
   return (
     <header className="header">
       <div className="logoContainer">
-        <img src={logo} alt="Logo" className="logo" />
+        <NavLink to={"/"}>
+          <img src={logo} alt="Logo" className="logo" />
+        </NavLink>
       </div>
       <div className="description">
         <h1 className="appName">{appDescription?.name}</h1>
         <p className="appDescription">{appDescription?.metadata.rowLabel}</p>
+      </div>
+      <div className="navigationContainer">
+        <NavLink to={"/chart"}>
+          Charts <img src={chart} alt="Chart" className="navigationLinksIcon" />
+        </NavLink>
+        <NavLink to={"/"}>
+          Map<img src={map} alt="Map" className="navigationLinksIcon" />
+        </NavLink>
       </div>
     </header>
   );
