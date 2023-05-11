09659a0a7a9ef7d9c1b6454192bd8896072a6179
Sorousch <soroush.raw@gmail.com>
2023-05-11 10:05:44 +0200

    Remove 'React.StrictMode' from src/index.tsx
    Revert: Remove React.StrictMode from src/index.tsx
    
    To reduce complexity, remove the unused
    React.StrictMode from src/index.tsx.

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -9,9 +8,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
